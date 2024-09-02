9ecdf8b67a44f7eb425937ced3b386ec716d175b
seolleung2 <seolleung22@gmail.com>
2024-09-02 20:32:59 +0900

    add Write Component, connect firebase database

--- src/components/Write.js
+++ src/components/Write.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { getDatabase, ref, set, push } from "firebase/database";
+import app from "../firebaseConfig";
 
 export default function Write() {
-  return <div>Write</div>;
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+
+  const saveData = () => {
+    const db = getDatabase(app);
+    const newDocRef = push(ref(db, "nature/fruits"));
+    set(newDocRef, {
+      fruitName: inputValue1,
+      fruitDefinition: inputValue2,
+    })
+      .then(() => {
+        alert("data saved successfully");
+      })
+      .catch((error) => {
+        alert("error: ", error.message);
+      });
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue1}
+        onChange={(e) => setInputValue1(e.target.value)}
+      />
+      <input
+        type="text"
+        value={inputValue2}
+        onChange={(e) => setInputValue2(e.target.value)}
+      />
+      <br />
+      <button onClick={saveData}>SAVE DATA</button>
+    </div>
+  );
 }
